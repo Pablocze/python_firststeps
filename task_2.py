79d383ea4b82e4231e4051171f2ff681cfba40c0
Pawel <pablos7@vp.pl>
2019-11-12 20:31:20 +0100

    task 1

---
+++ task_2.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+import unittest
+from group import Group
+
+def is_alert_present(self):
+    try:
+        self.wd.switch_to_alert()
+    except NoAlertPresentException as e:
+        return False
+    return True
+
+
+class Task1(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/group.php")
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_id("LoginForm").click()
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def group_creation(self, wd, group):
+        # group creation initialization
+        wd.find_element_by_name("new").click()
+        # filling group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def test_task1(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.group_creation(wd, Group(name="name", header="noidea", footer="whatimdoing"))
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
